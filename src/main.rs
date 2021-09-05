b90203fae0ca50b6abbd5f0748b8960a4ae49ce5
geauxvirtual <justin.guidroz@gmail.com>
2021-09-05 10:53:26 -0700

    Configure initial logging setup to stdout

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,22 @@
 // be able to be exported to a .fit file for uploading to external applications
 // or sites.
 
-fn main() {}
+fn main() {
+    // Configure application level logging.
+    // TODO: Switch from stdout logging to logging to file once the
+    // application is stable enough.
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Info)
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
+}
