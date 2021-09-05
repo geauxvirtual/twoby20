275cd0a50da777b9d93b7763d65d3d1c21d63c1b
geauxvirtual <justin.guidroz@gmail.com>
2021-09-05 11:09:41 -0700

    Allow setting application log level from command line

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,27 @@
 // on screen based on a predescribed workout. The workout will be recorded and
 // be able to be exported to a .fit file for uploading to external applications
 // or sites.
+use std::str::FromStr;
+
+use clap::{App, Arg};
+
+// Configure command line options for the application.
+fn app() -> App<'static, 'static> {
+    App::new("2by20").arg(
+        Arg::with_name("log-level")
+            .short("l")
+            .long("log-level")
+            .value_name("LOG_LEVEL")
+            .takes_value(true),
+    )
+}
 
 fn main() {
+    // Read command line options
+    let matches = app().get_matches();
+    // Default application logging to info
+    let log_level = matches.value_of("log-level").unwrap_or("info");
+    let log_level_filter = log::LevelFilter::from_str(log_level).unwrap();
     // Configure application level logging.
     // TODO: Switch from stdout logging to logging to file once the
     // application is stable enough.
@@ -19,7 +38,7 @@ fn main() {
                 message
             ))
         })
-        .level(log::LevelFilter::Info)
+        .level(log_level_filter)
         .chain(std::io::stdout())
         .apply()
         .unwrap();
