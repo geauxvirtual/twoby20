ee48673a7e21861010ff3f8b5cbd815fd059e553
geauxvirtual <justin.guidroz@gmail.com>
2021-09-05 10:07:00 -0700

    And away we go...

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// 2by20 is an indoor cycling training application with a simple of goal
+// recoding an indoor training ride reading data from devices such as power
+// meters, heart rate monitors, cadence sensors, etc.  and displaying this data
+// on screen based on a predescribed workout. The workout will be recorded and
+// be able to be exported to a .fit file for uploading to external applications
+// or sites.
+
+fn main() {}
