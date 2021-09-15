ab5f5afa62de31730362137322adca9d989f0a10
geauxvirtual <justin.guidroz@gmail.com>
2021-09-15 08:56:00 -0700

    Update how to call libant::run()

--- src/application.rs
+++ src/application.rs
@@ -5,20 +5,25 @@
 // with data being sent between threads via channels.
 use std::thread;
 
-use libant::{Ant, Context};
-
 // Run() is the main function to call. This handles starting up all the
 // threads and configuring the channels.
 pub fn run() {
     let (_ant_request_tx, ant_request_rx) = libant::unbounded();
-    let (ant_message_tx, _ant_message_rx) = libant::unbounded();
+    let (ant_message_tx, ant_message_rx) = libant::unbounded();
 
-    let mut context = Context::new().unwrap();
-    let mut ant = Ant::init(&mut context, ant_request_rx, ant_message_tx).unwrap();
+    let ant_run_handle = thread::spawn(move || libant::ant::run(ant_request_rx, ant_message_tx));
 
-    let ant_run_handle = thread::spawn(move || {
-        let _ = ant.run();
-    });
-
+    loop {
+        match ant_message_rx.recv() {
+            Ok(libant::Response::Error(e)) => {
+                log::error!("Error message received: {:?}", e);
+            }
+            Ok(mesg) => log::debug!("Debugging message received: {:?}", mesg),
+            Err(e) => {
+                log::error!("Error receiving from Ant run thread: {:?}", e);
+                break;
+            }
+        }
+    }
     ant_run_handle.join().unwrap();
 }
