fd22d5b863f5717ab5725afa39d97688399b604c
geauxvirtual <justin.guidroz@gmail.com>
2021-10-29 15:31:55 -0700

    Add ui.rs and ui/menubar.rs

---
+++ src/application/ui.rs
@@ -0,0 +1,6 @@
+// All UI elements for twoby20 are found under application/ui.
+// Each module contains the separate UI elements for the entire
+// application.
+
+pub mod menubar;
+pub mod user_profile;
